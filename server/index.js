fa500f717134adf17b6aa5bdc00010281f9e8518
ashiqurras <ashiqurrussel2@gmail.com>
2022-02-13 19:28:18 +0100

    Heroku server up

--- server/index.js
+++ server/index.js
@@ -1,41 +1,34 @@
 const express = require("express")
 const app = express()
-const data = require("./data/index.json")
 const cors = require("cors")
+const data = require('./data/index.json');
 
+let port = process.env.PORT || 8000;
+
+//Middlewire for cors policy
 app.use(cors())
-
-const port = process.env.PORT || 8000;
-
-// generate random number
+// generating random values between 400 and 3500
 const genRandom = (min, max) => {
     return Math.floor(Math.random() * max) + min;
 }
 const randomCo2 = () => {
     return genRandom(400, 3500);
 };
-
-// console.log(data[0].sensorDetails[0].CO2)
-
+//Default route
+app.get("/", (req, res) => {
+    res.send("Welcome to the Server")
+})
 //Routes
 app.get("/data", (req, res) => {
-    const newData = [];
-    var i = 0;
+    var newData = []
     for (const key of data) {
         // Update CO2 value with timestamp
-        data[i].sensorDetails.forEach(item => {
-            const newCo2 = randomCo2();
-            const newTime = new Date().getTime();
-            item.CO2 = newCo2;
-            item.timestamp = newTime;
-            newData.push(newCo2);
-        });
-        i++;
+        // var item = data[arr[key]];
+        key.co2 = randomCo2();
+        key.timestamp = new Date().getTime();
+        newData.push(key);
     }
-    res.send(data)
-})
+    res.send((newData));
+});
 
-//Server listening port
-app.listen(port, (req, res) => {
-    console.log(`Server Listening on port ${port}`)
-})
+app.listen(port => { });
