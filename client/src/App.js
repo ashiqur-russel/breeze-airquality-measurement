ba1826e643be753471f6198901e2243bd4bf5349
ashiqurras <ashiqurrussel2@gmail.com>
2022-02-13 12:03:17 +0100

    Map API load from Mapbox

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Api from "./Api/Api"
+
+function App() {
+  return (
+    <>
+      <Api />
+
+    </>
+  )
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 const data = require("./data/index.json")
+const cors = require("cors")
+
+app.use(cors())
 
 const port = process.env.PORT || 8000;
 
